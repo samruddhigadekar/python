7bdcb3a5145655bb00ba1d804fd3e2a34b26b497
Samruddhi Gadekar <138895008+samruddhigadekar@users.noreply.github.com>
2024-07-01 20:18:18 +0530

    Add files via upload

---
+++ per.py
@@ -0,0 +1,15 @@
+a=input("enter per")
+a=float(a)
+if a>=80:
+    print("result:O")
+elif a>=70:
+    print("result:A+")
+elif a>=60:
+    print("result:A")
+elif a>=50:
+    print("result:B")
+elif a>=40:
+    print("result:PASS")
+else:
+    print("FAIL")
+    
